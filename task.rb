c9164b50e4628681cbd5bf7ee298762ca47c581c
michael.remley <Mikejamesremley@gmail.com>
2019-05-23 10:42:43 -0700

    Formated task.rb to enhance readability

--- task.rb
+++ task.rb
@@ -1,8 +1,12 @@
 ## TaskManager Module written by: Michael Remley
 ## For use in the CST223 Final Project
 module TaskManager
-  #Task Interface
-  #Giving structure to tasks
+  '''***************************************
+  Class:    Task
+
+  Purpose:  Provide an "inteface" for which
+            all tasks derive from.
+  ***************************************'''
   class Task
     #task constructor
     def initialize(id, item, priority, due)
@@ -11,33 +15,52 @@ module TaskManager
       @priority = priority
       @due = due
     end
+
     #Extend the to_s for
     #printing with a given task
     def to_s
       print "Task: ", @item
     end
+
     def getItem
       @item
     end
   end #End TaskInterface
 
+  '''***************************************
+  Class:    Feature
+
+  Purpose:  A type of task
+
+  Use:      Define a feature
+
+  # TODO:   Possible addition of "on click"
+            information for a feature
+  ***************************************'''
   class Feature < Task
     #class 'constructor'
     def initialize(id, item, priority, due)
       super(id, item, priority, due)
     end
+
     def to_s
       print "Feature: ", @item
     end
   end #End Feature < TaskInterface
 
-  #List class for dealing with a
-  #list of tasks
+  '''***************************************
+  Class:    List
+
+  Purpose:  To Provide list functionality
+            for use with the super class
+            "Task".
+  ***************************************'''
   class List
     #List 'constructor'
     def initialize
       @tasks = Array.new
     end
+
     #To add a task to the list
     def append(aTask)
       if aTask.kind_of?(Task) #looks for super task
@@ -47,30 +70,39 @@ module TaskManager
         raise "not a task"
       end
     end
+
     #remove the first task
     def deleteFirst
       @tasks.shift
     end
+
     #remove the last task
     def deleteLast
       @tasks.pop
     end
+
     #overload of the [] operator
     def [](key)
       if key.kind_of?(Integer) #looks for superclass Integer
         result = @tasks[key]
       else #get task by name
-        result = @tasks.find { |aTask| key == aTask.getItem}
+        result = @tasks.find { |aTask| key == aTask.getItem }
       end
       return result
     end
+
     #remove a task with a supplied index
     def deleteAt(index)
       @tasks.delete_at(index)
     end
   end #End class List
 
-  #for use in displaying a list of tasks
+  '''***************************************
+  Class:    Window
+
+  Purpose:  Provide formating functionality
+            to a list of tasks.
+  ***************************************'''
   class Window
     #constructor
     def initialize(list)
@@ -79,7 +111,6 @@ module TaskManager
       else
         raise "not a list"
       end
-    end
+    end #End Init
   end #End Window class
-
 end #End Module
